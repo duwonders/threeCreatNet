7cce804ac80f8cc53fb8b389aae04701d467ca10
Hangeer <591417089@qq.com>
2016-08-06 20:01:37 +0800

    Test add backend and upload

---
+++ src/backend/controller/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import Base from './base.js';
+
+import fs from 'fs';
+import path from 'path'
+
+export default class extends Base {
+  /**
+   * index action
+   * @return {Promise} []
+   */
+  indexAction(){
+    //auto render template file index_index.html
+    return this.display();
+  }
+  uploadAction (){
+    //这里的 key 需要和 form 表单里的 name 值保持一致
+    let file = think.extend({}, this.file('image'));
+
+    let filepath = file.path;
+
+    //文件上传后，需要将文件移动到项目其他地方，否则会在请求结束时删除掉该文件
+    let uploadPath = think.RESOURCE_PATH + '/upload';
+    think.mkdir(uploadPath);
+    
+    let basename = path.basename(filepath);
+    fs.renameSync(filepath, uploadPath + '/' + basename);
+
+    file.path = uploadPath + '/' + basename;
+
+    if(think.isFile(file.path)){
+      console.log('is file')
+    }else{
+      console.log('not exist')
+    }
+
+    this.assign('fileInfo', file);
+    // this.display();
+  }
+}
