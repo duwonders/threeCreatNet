356cd70e0510507d1afc3480d59ae540de4bbedc
Hangeer <591417089@qq.com>
2016-08-09 12:07:38 +0800

    添加一些注释

--- src/backend/controller/index.js
+++ src/backend/controller/index.js
@@ -16,13 +16,11 @@ export default class extends Base {
   }
   uploadAction (){
     this.setCorsHeader();
-    // 设置允许外部 请求
 
     //这里的 key 需要和 form 表单里的 name 值保持一致
     let file = think.extend({}, this.file('image'));
 
     let filepath = file.path;
-
     //文件上传后，需要将文件移动到项目其他地方，否则会在请求结束时删除掉该文件
     let uploadPath = think.RESOURCE_PATH + '/upload';
     think.mkdir(uploadPath);
@@ -42,28 +40,34 @@ export default class extends Base {
     this.success(basename);
     //  this.suceess 用来返回值 vue 中 post().then 接收 返回链接
   }
-  setCorsHeader(){ this.header("Access-Control-Allow-Origin", this.header("origin") || "*"); this.header("Access-Control-Allow-Headers", "x-requested-with"); this.header("Access-Control-Request-Method", "GET,POST,PUT,DELETE"); this.header("Access-Control-Allow-Credentials", "true");
-    //  设置 header
-  }
 
+  setCorsHeader(){
+      this.header("Access-Control-Allow-Origin", this.header("origin") || "*");
+      this.header("Access-Control-Allow-Headers", "x-requested-with");
+      this.header("Access-Control-Request-Method", "GET,POST,PUT,DELETE");
+      this.header("Access-Control-Allow-Credentials", "true");
+  }
+  /*
+  *  设置允许请求的 header
+  *  调试不同端口用
+  */
+
+  /* 下面是数据库操作 */
 
   /*
-  * 数据库操作
   * gallery 页面
+  *    |
+  *    |----album 路由(/gallery/album)
+  *    |
+  *    |----image 路由(/gallery/image)
   */
   async getpicAction(){
     this.setCorsHeader();
-    // 设置允许外部 请求
 
-    // let data = await this.model('picture').select();
     let json = this.post();
-
     let data = await this.model('picture').page([json.currentPage, 5]).countSelect();
     //  分页的查询 .page([现页数， 总页数])
-
-    // console.log(data);
     console.log("获取图片列表 data");
-
     return this.success(data);
   }
   /* 获取图片的信息 */
@@ -77,7 +81,6 @@ export default class extends Base {
     let isAlbumExist = await album
         .where({id: json.album_id})
         .find();
-
     if (!think.isEmpty(isAlbumExist)) {
         let set_album_pages = parseInt(isAlbumExist.album_pages) + 1;
         let affactedRows = await album
@@ -104,8 +107,6 @@ export default class extends Base {
       let pic_id_obj = this.post();
       let picture = this.model('picture');
       let album = this.model('album');
-
-
       let album_id = (await picture.where(pic_id_obj).find()).album_id;
       let set_album_pages = (await album.where({id: album_id}).find()).album_pages - 1;
       let affectedRows = await album
@@ -113,7 +114,6 @@ export default class extends Base {
           .update({album_pages: set_album_pages});
       let retData = await picture.where({id: ["=", pic_id_obj.id]}).delete();
       return this.success(retData);
-
       /*
       *  单张删除图片
       *  不用判断 因为相册 id 肯定是存在的
@@ -127,74 +127,62 @@ export default class extends Base {
 
       let data = this.post();
       let model = this.model('picture');
-
-      //console.log(data);
-
       let retData = await model.where({id: data.id}).update({
          album_id: data.album_id,
          pic_name: data.pic_name,
          pic_url: data.pic_url
       });
-
       return this.success(retData);
   }
   /* 更新单张图片信息 */
-
 
   async getalbumAction(){
     this.setCorsHeader();
 
     let json = this.post();
-
     let data = await this.model('album').page([json.currentPage, 5]).countSelect();
-    //  分页的查询 .page([现页数， 总页数])
-    //console.log(data);
     return this.success(data);
   }
   /* 获取相册的信息 */
+
   async postalbumAction(){
     this.setCorsHeader();
-      // 设置允许外部 请求
+
     let json = this.post();
     let model = this.model('album');
     let retData = await model.add(json);
-
     return this.success(retData);
   }
   /* 上传相册信息 */
+
   async deletesinglealbumAction () {
       this.setCorsHeader();
 
       let album_id_obj = this.post();
       let album = this.model('album');
       let picture = this.model('picture');
-
       let affectedRows = await picture.where({album_id: ["=", album_id_obj.id]}).delete();
       let retData = await album.where({id: ["=", album_id_obj.id]}).delete();
+      return this.success(retData);
       /*
       *  删除相册的时候 把相册中图片的对应关系也一并删除
       *  当然图片文件还是在里面的
       */
-
-      return this.success(retData);
   }
   /* 删除相册 */
+
   async updatesinglealbumAction () {
       this.setCorsHeader();
-      //  设置允许外部 请求
+
       let data = this.post();
       let model = this.model('album');
-
-    //   console.log(data);
-      console.log("删除单张图片信息 data");
-
       let retData = await model.where({id: data.id}).update({
          album_name: data.album_name,
          album_cover: data.album_cover
       });
-
       return this.success(retData);
   }
   /* 更新相册信息 */
+
 
 }
