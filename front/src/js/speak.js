3925a2ff034a63506f153026529668247658aa0e
Hangeer <591417089@qq.com>
2016-08-27 21:48:24 +0800

    我语科联 分页 完成

--- front/src/js/speak.js
+++ front/src/js/speak.js
@@ -36,7 +36,66 @@ $(document).ready(() => {
                 i.addClass('current');
             }
         });
+        $('.page-outer-container').css('display', 'block');
     }
-    //  设置分页器的样式啥的
+    //  设置分页器的样式
     pagersSet();
+
+    let gotoPage = (() => {
+        let cache = {};
+        return function (page, type = GetQueryString('type')) {
+            cache.type = type || '最近';
+            cache.page = page || 1;
+            window.location.href = `/speak?type=${type}&page=${page}`;
+        }
+    })();
+    /**
+    *   gotoPage 用来跳转连接
+    */
+
+    $('.top-page').bind('click', (e) => {
+        e = e || window.event;
+        if (e.target.nodeName.toLowerCase() == 'a') {
+            e.preventDefault();
+            let type = $(e.target).text().trim().slice(0, 2);
+            gotoPage(1, type);
+        }
+    });
+    //  点击分类按钮
+
+    $('.page-outer-container').bind('click', (e) => {
+        e = e || window.event;
+        if (e.target.className == 'pager') {
+            let t = e.target;
+            let page = ~~$(e.target).text();
+            if (page > 0) {
+                gotoPage(page);
+            }
+        }
+    });
+    //  点击分页按钮
+
+    $('#prev').bind('click', () => {
+        let page = ~~GetQueryString('page');
+        let toPage = page - 1;
+
+        page == 1 ? alert(`已经是第一页`) : gotoPage(toPage);
+    });
+    //  点击上一页按钮
+
+    $('#next').bind('click', () => {
+        let page = ~~GetQueryString('page');
+        let totalPage = ~~$("#total-page").text();
+        let toPage = page + 1;
+
+        page == totalPage ? alert(`已经是最后一页`) : gotoPage(toPage);
+    });
+    //  点击下一页按钮
+
+    $("#page-go").bind('click', () => {
+        let totalPage = ~~$("#total-page").text();
+        let toPage = ~~$("#page-input").val();
+
+        toPage >= 1 && toPage <= totalPage ? gotoPage(toPage) : alert(`无效的页数`);
+    });
 });
--- src/home/controller/speak.js
+++ src/home/controller/speak.js
@@ -14,20 +14,27 @@ export default class extends Base {
 
     this.assign('data', data);
 
-    console.log(data.getArticle.data);
-
     return this.display();
   }
 
   async get_article () {
       let page = this.get('page') || 1;
-      let type = this.get('type') || '最近上传';
+      let type = this.get('type') || '最近';
 
       let article = this.model('article');
+      let res = {};
 
-      let res = await article.page(page, 15)
-                        .order("id DESC")
-                        .countSelect();
+      if (type == '最近') {
+          res = await article.page(page, 10)
+                            .order("id DESC")
+                            .countSelect();
+      } else {
+          res = await article.where({atc_type: type})
+                            .page(page, 3)
+                            .order("id DESC")
+                            .countSelect();
+      }
+
       return res;
   }
 }
