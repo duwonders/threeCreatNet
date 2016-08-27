6dfbe65724708f018cf07f86712a3004cbe39734
Hangeer <591417089@qq.com>
2016-08-27 19:49:11 +0800

    我语科联页面前端 添加分页器效果

--- front/src/js/speak.js
+++ front/src/js/speak.js
@@ -4,5 +4,39 @@ $(document).ready(() => {
          var r = window.location.search.substr(1).match(reg);
          if(r!=null)return  decodeURI(r[2]); return null;
     }
-    
+
+    function pagersSet () {
+        let totalPage = $("#total-page").text(),
+            currentPage = $("#current-page").text();
+        let $pagers = $('.pager');
+        if (totalPage < 5) {
+            for (let i = 0; i < 5; i++) {
+                if (totalPage < i+1) {
+                    $($pagers[i]).css('display', 'none');
+                }
+            }
+        } else if (totalPage > 5) {
+            if (totalPage > 6 && currentPage > 3 && currentPage < totalPage - 2) {
+                $($pagers[1]).text(`...`);
+                $($pagers[2]).text(currentPage);
+                $($pagers[3]).text(`...`);
+            }
+            if (currentPage <= 3) {
+                $($pagers[3]).text(`...`);
+            } else if (totalPage - currentPage <= 2) {
+                $($pagers[1]).text(`...`);
+                $($pagers[2]).text(totalPage - 2);
+                $($pagers[3]).text(totalPage - 1);
+            }
+            $($pagers[4]).text(totalPage);
+        }
+        $pagers.map((index, item) => {
+            let i = $(item);
+            if (~~i.text() == ~~currentPage) {
+                i.addClass('current');
+            }
+        });
+    }
+    //  设置分页器的样式啥的
+    pagersSet();
 });
