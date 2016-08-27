dba4ccd8c679b3cbf3b9bbf3a294f1f359821b68
Hangeer <591417089@qq.com>
2016-08-27 17:24:28 +0800

    开始渲染 我语科联 界面 处理了一些样式

--- src/home/controller/speak.js
+++ src/home/controller/speak.js
@@ -7,7 +7,27 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  async indexAction(){
+    let data = {
+        getArticle: await this.get_article()
+    };
+
+    this.assign('data', data);
+
+    console.log(data.getArticle.data);
+
     return this.display();
+  }
+
+  async get_article () {
+      let page = this.get('page') || 1;
+      let type = this.get('type') || '最近上传';
+
+      let article = this.model('article');
+
+      let res = await article.page(page, 15)
+                        .order("id DESC")
+                        .countSelect();
+      return res;
   }
 }
