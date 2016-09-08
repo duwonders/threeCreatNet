29a095d2d398c74976860190934c5ce7cff19ed7
duwonders <duwonders@gmail.com>
2016-09-08 18:32:56 +0800

    dsa

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -8,9 +8,86 @@ export default class extends Base {
    * @return {Promise} []
    */
   async indexAction(){
+    let data = {
+
+      newActivity: {
+
+        topAll: await this.getActivity(),
+
+        competitions: await this.getCompetition(),
+
+        speeach: await this.getSpeech()
+
+      },
+
+      album: await this.getAlbum(),
+
+      download: await this.getDownload(),
+
+    }
+
+    this.assign('data', data);
+
     return this.display();
   }
 
+  //根据活动上传时间排名
+  async getCompetition(){
+    let data = await this.model('hd')
+                     .limit(3)
+                     .where({
+                       hd_type: '竞赛'
+                     })
+                     .order('hd_time')
+                     .select();
+
+    return data;
+  }
+
+  async getSpeech(){
+    let data = await this.model('hd')
+                     .limit(3)
+                     .where({
+                       hd_type: '讲座'
+                     })
+                     .order('hd_time')
+                     .select();
+
+    return data;
+  }
+
+  async getActivity(){
+    let activity = this.controller('activity'),
+
+        data = await this.model('hd')
+                         .limit(7)
+                         .order('hd_time')
+                         .select();
+
+        return data;
+  }
+
+
+  //根据相册热门度排名
+  async getAlbum(){
+
+    let data = await this.model('album')
+                         .limit(6)
+                         .order('album_likes')
+                         .select();
+
+
+        return data;
+  }
+
+  async getDownload(){
+    let data = await this.model('download')
+                         .limit(14)
+                         .order('file_time')
+                         .select();
+
+        return data;
+  }
   /**
   *  16-08-22
   *  加了搜索页面的 display
