c68a5c898fdf108abeb22f275a83b580ebf0d59b
duwonders <duwonders@gmail.com>
2016-09-08 15:00:56 +0800

    dsa

--- src/home/controller/album.js
+++ src/home/controller/album.js
@@ -10,7 +10,7 @@ export default class extends Base {
   async indexAction(){
   	let data = await this.model('album')
   											 .select();
-    
+
     this.assign('data', data);
 
     console.log(data);
@@ -18,4 +18,48 @@ export default class extends Base {
     return this.display();
   }
 
+  async imagefAction(){
+    let data = {};
+
+        data.pic = await this.model('picture')
+                         .where({
+                          album_id: this.get('id'),
+                         })
+                         .select();
+        data.album = await this.model('album')
+                               .where({
+                                 id: this.get('id'),
+                               })
+                               .select(); 
+    console.log(data);
+
+    this.assign('data', data);                     
+
+    return this.display();
+  }
+
+  async voteAction(){
+    let picid = this.get('id'),
+
+        obj = await this.model('picture')
+                        .where({
+                          id: picid
+                        })
+                        .select(),
+
+        album_id = obj[0].album_id;
+
+    this.model('picture')
+        .where({
+          id: picid
+        })
+        .increment('pic_likes');
+
+    this.model('album')
+        .where({
+          id: album_id
+        })
+        .increment('album_likes')
+    this.json({status: 'ok'});
+  }
 }
