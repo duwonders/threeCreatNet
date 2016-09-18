19582b54d3ca9f3ed07cff8a3f856a0126149d7a
Hangeer <591417089@qq.com>
2016-09-18 15:05:25 +0800

    清缓存 改 gitignore

--- src/common/config/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/**
- * config
- */
-export default {
-    resource_on: true, //是否开启静态资源解析功能
-    resource_reg: /^(upload\/|[^\/]+\.(?!ppt|doc|docx|jpeg|jpg|png|xls)\w+$)|(static\/*)/, //判断为静态资源请求的正则
-};
